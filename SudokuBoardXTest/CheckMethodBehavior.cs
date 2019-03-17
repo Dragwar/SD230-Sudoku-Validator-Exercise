db85a8d9954c85f7d2593e2859a56e6db36c5f25
Everett Grassler <dragwar9@gmail.com>
2019-03-17 03:12:00 -0500

    Small re-factor merged similar tests

--- SudokuBoardXTest/CheckMethodBehavior.cs
+++ SudokuBoardXTest/CheckMethodBehavior.cs
@@ -6,36 +6,21 @@ namespace SudokuBoardXTest
 {
     public class CheckMethodBehavior
     {
-        #region GetRowInvalidRange Tests
         [Fact]
-        public void GetRowInvalidRange_1()
+        public void GetRowInvalidRange()
         {
             SudokuBoardValidator validator = new SudokuBoardValidator(SudokuBoards.ValidBoard);
             Assert.Throws<ArgumentOutOfRangeException>(() => validator.GetRow(-1));
-        }
-
-        [Fact]
-        public void GetRowInvalidRange_2()
-        {
-            SudokuBoardValidator validator = new SudokuBoardValidator(SudokuBoards.ValidBoard);
             Assert.Throws<ArgumentOutOfRangeException>(() => validator.GetRow(43));
         }
-        #endregion
 
-        #region GetColumnInvalidRange Tests
         [Fact]
-        public void GetColumnInvalidRange_1()
+        public void GetColumnInvalidRange()
         {
             SudokuBoardValidator validator = new SudokuBoardValidator(SudokuBoards.ValidBoard);
             Assert.Throws<ArgumentOutOfRangeException>(() => validator.GetColumn(-1));
-        }
-        [Fact]
-        public void GetColumnInvalidRange_2()
-        {
-            SudokuBoardValidator validator = new SudokuBoardValidator(SudokuBoards.ValidBoard);
             Assert.Throws<ArgumentOutOfRangeException>(() => validator.GetColumn(35));
         }
-        #endregion
 
 
     }
